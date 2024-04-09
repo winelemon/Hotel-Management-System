ff47c485c0f5c17a45c035617fc780d7b8c00a74
muteaool <11525851+muteaool@user.noreply.gitee.com>
2024-04-09 10:36:50 +0800

    hotel_user

---
+++ hotel_user/config/prod.env.js
@@ -0,0 +1,8 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  // BASE_API: '"http://localhost:8080/ho-api"',
+  //BASE_API: '"http://localhost:8080"',
+  BASE_API: '"http://121.41.226.28:8080"',
+
+  }
